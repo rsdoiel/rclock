92329d2ee23a251bf7862965f5fd60debf657240
R. S. Doiel <rsdoiel@gmail.com>
2013-02-22 06:49:07 -0800

    added weather data source via YQL.

--- htdocs/js/rclock.js
+++ htdocs/js/rclock.js
@@ -1,18 +1,30 @@
-
+//
+// rclock.js
+//
 /*jslint browser: true */
 /*global YUI */
-YUI().use("node", "datatype-date", "io", function (Y) {
-	"use strict";
-	var weather = Y.one("#weather"),
-		clock = Y.one("#clock"),
-		alarms = Y.one("alarms");
+YUI().use("node", "io", "datatype-date",  "yql", function (Y) {
+	var clock = Y.one("#clock"),
+		pre = Y.one("pre"),
+		clock_rate = 1000,
+		weather = Y.one("#weather"),
+		alarms = Y.one("#alarms");
 
-	Y.log("Setting up clock and weather feed", "info");
-	clock.set("text", Y.Date.format(new Date(), {format: "%T %p"}));
-
+	//Y.YQL('select latitude, longitude, uzip, woeid, city, county, state from geo.placefinder where name = "91350"', function (geo_r) {
+	Y.YQL('select woeid from geo.placefinder where name = "91350"', function (geo_r) {
+		var qry = [
+			'select * from weather.forecast where woeid=',
+			geo_r.query.results.Result.woeid
+		].join("");
+		Y.YQL(qry, function (weather_r) {
+			pre.set('text', JSON.stringify(weather_r.query.results.channel.item.forecast).replace(/},{/g, "},\n{"));
+		});
+	});
+	// FIXME: Need to only run the setInterval if the page
+	// is visible. Check the Moz APIs for their Firefox OS
+	// platform to see how to keep from burning battery
+	// unnecessarily
 	setInterval(function () {
-		clock.set("text", Y.Date.format(new Date(), {format: "%T %p"}));
-	}, 1000);
-
-
+		clock.setHTML(Y.Date.format(new Date(), {format: "%H:%I:%S %p"}));
+	}, clock_rate);
 });
