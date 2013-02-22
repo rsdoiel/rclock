b551c9fd5eeec4bad45214bb5ca634ff71d07c1a
R. S. Doiel <rsdoiel@gmail.com>
2013-02-21 22:21:09 -0800

    snapshot

---
+++ htdocs/js/ralarmclock.js
@@ -0,0 +1,13 @@
+
+/*jslint browser: true */
+/*global YUI */
+YUI().use("node", "datetype-date", "io", function (Y) {
+	"use strict";
+	var weather = Y.one("#weather"),
+		clock = Y.one("#clock"),
+		alarms = Y.one("alarms");
+
+	Y.log("Hello World!", "info");
+	clock.set("text", Y.Date.format(new Date(), {format: "%T %p"}));
+
+}):
