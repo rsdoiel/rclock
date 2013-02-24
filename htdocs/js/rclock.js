7cd1555e0973a9580c689fb0ec784f7343f2d855
R. S. Doiel <rsdoiel@gmail.com>
2013-02-24 10:53:05 -0800

    snapshot

--- htdocs/js/rclock.js
+++ htdocs/js/rclock.js
@@ -3,21 +3,38 @@
 //
 /*jslint browser: true */
 /*global YUI */
-YUI().use("node", "io", "datatype-date",  "yql", function (Y) {
-	var clock = Y.one("#clock"),
+YUI().use("node", "io", "datatype-date", "yql", "template", function (Y) {
+	var micro = new Y.Template(),
+		clock = Y.one("#clock"),
 		pre = Y.one("pre"),
 		clock_rate = 1000,
 		weather = Y.one("#weather"),
+		location = Y.one("#location"),
+		forecase = Y.one("#forecast"),
 		alarms = Y.one("#alarms");
 
 	//Y.YQL('select latitude, longitude, uzip, woeid, city, county, state from geo.placefinder where name = "91350"', function (geo_r) {
-	Y.YQL('select woeid from geo.placefinder where name = "91350"', function (geo_r) {
+	Y.YQL('select woeid, city from geo.placefinder where name = "91350"', function (geo_r) {
+		var location_info = geo_r.query.results.Result || {};
+		var forecastLI = "<li>{day} {high}/{low} {text}</li>";
 		var qry = [
 			'select * from weather.forecast where woeid=',
 			geo_r.query.results.Result.woeid
 		].join("");
+		location.set('text', location_info.neighborhood || location_info.city || location_info.county);
 		Y.YQL(qry, function (weather_r) {
-			pre.set('text', JSON.stringify(weather_r.query.results.channel.item.forecast).replace(/},{/g, "},\n{"));
+			var forecasts = weather_r.query.results.channel.item.forecast || {},
+				output = [];	
+
+			location.append(Y.Lang.sub(forecastLI, {
+				day: "<b>Day</b>",
+				high: "<b>Hi</b>",
+				low: "<b>Lo</b>",
+				text: "<b>forecase</b>"
+			}));	
+			Y.Array.each(forecasts, function (forecast) {
+				location.append(Y.Lang.sub(forecastLI, forecast));
+			});
 		});
 	});
 	// FIXME: Need to only run the setInterval if the page
